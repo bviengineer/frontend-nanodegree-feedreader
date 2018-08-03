d5d69c47631c3826498082930dab5f4a1417569e
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-08-03 01:20:40 -0400

    Accurately test for the name field to be defined and not be empty for each each object in the allFeeds array

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,6 +43,14 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it("feed has a name", function(){
+            for(let i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].name).toBeDefined;
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+        });
+
     });
 
 
